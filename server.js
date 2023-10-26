5c070ad63bfed51d53c5735e5eebf6a9202810dd
Junior01112000 <j.kouakou@epsi.fr>
2023-10-26 20:45:54 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/JuicyWebSite'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/JuicyWebSite/index.html'));});
+    res.sendFile(path.join(__dirname+'/dist/juicy-web-site/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
